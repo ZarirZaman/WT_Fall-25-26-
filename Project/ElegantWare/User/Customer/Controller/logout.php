eb358ed21eef640fdbfa89b6de88310c40943544
ZarirZaman <130787488+ZarirZaman@users.noreply.github.com>
2026-01-09 05:00:52 +0600

    fix

--- Project/ElegantWare/User/Customer/Controller/logout.php
+++ Project/ElegantWare/User/Customer/Controller/logout.php
@@ -1,13 +1,8 @@
 <?php
-// Include config to start session
 require_once '../Model/config.php';
-
-// Include auth functions
 require_once '../Model/auth.php';
 
-// Call the logout function from the model
 logoutUser();
 
-// Redirect to login page
 redirect('login.php');
 ?>
